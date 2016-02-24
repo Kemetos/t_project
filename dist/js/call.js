b5e5bf13b9b7a9464c10b3815ceaea95d0744fce
Thibaud Camhaji <Thib@mbpt.local>
2016-02-24 22:20:47 +0100

    change phone

--- dist/js/call.js
+++ dist/js/call.js
@@ -1,3 +1,11 @@
 $('.dialpad ul li p:first-child').on("click", function(){
-    TweenMax.fromTo($(this), 1, {css: {backgroundColor: "rgba(255,255,255,1)", color: "#d5ff00"}}, {css:{backgroundColor: "rgba(255,255,255,0)", color: "#fff"}} )
+    TweenMax.fromTo($(this), 1, {css: {backgroundColor: "rgba(255,255,255,1)", color: "#d5ff00"}}, {css:{backgroundColor: "rgba(255,255,255,0)", color: "#fff"}} );
+
+    console.log($(this)[0].innerHTML);
+    $('.right_call h3').append($(this)[0].innerHTML)
 });
+
+var switchPhone = function(){
+  $('.phoneCard').css("display","none");
+  $('.tapDial').css("display","block");
+};

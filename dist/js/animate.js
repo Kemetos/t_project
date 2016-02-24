4ad70966bff181cbb89e417a780af8dafa295b81
Rodolphe Grivet <Rodolphe Grivet>
2016-02-24 20:12:42 +0100

    add animation

--- dist/js/animate.js
+++
@@ -1,3 +0,0 @@
-var bigCard = document.getElementsByClassName("big-screen")[0];
-
-TweenMax.to(bigCard, 2, {left:"700px", repeat:10, yoyo:true, onRepeat:onRepeat, repeatDelay:0.5, ease:Linear.easeNone});

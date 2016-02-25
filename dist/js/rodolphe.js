11de45b2126269c6a33b0bef235b587ed26286d1
Rodolphe Grivet <Rodolphe Grivet>
2016-02-25 09:58:32 +0100

    counter

--- dist/js/rodolphe.js
+++ dist/js/rodolphe.js
@@ -22,3 +22,15 @@ var switchPlay = function(){
     $(".play").toggleClass("hide");
     $(".pause").toggleClass("show");
 }
+
+$('.number').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
